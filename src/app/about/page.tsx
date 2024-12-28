1c743b2a24f375da7a9b0a56255723b02f6f9c41
mehakkhan123 <mahekkhan284@gmail.com>
2024-12-28 20:51:57 +0500

    updated about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ const montserrat = Montserrat({subsets:["latin"]})
 
 export default function About() {
   return (
-    <div className='flex flex-col justify-center items-center p-8'>
+    <div className={`${montserrat.className} flex flex-col justify-center items-center p-8`}>
       <h1 className='text-2xl font-bold text-gray-500 lg:text-4xl max-w-[1322px] '>About Us</h1>
       <p className='text-center py-5 max-w-[1322px] '>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi ea repellat distinctio adipisci quam incidunt, veritatis reiciendis maxime molestiae explicabo dignissimos vitae recusandae animi fuga magni dolores nam? Ullam, architecto.
       </p>
